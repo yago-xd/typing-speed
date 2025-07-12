97b9152ec8ee83db6b9d202e209d5d441cd196b8
yago-xd <srijanbhadra41@gmail.com>
2025-07-12 23:42:03 +0530

    Almost all features except actual wpm work. Added accuracy as both parts, Dynamically updating test countdown. Word counting has been changed.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.util.Random;
 public class Main {
     static Scanner sc = new Scanner(System.in);
     static Random rand = new Random();
-    static float time,wpm,accuracy;
+    static float time, accuracy_total, accuracy_input;
+    static int raw_wpm,final_wpm;
     static String[] sentences = {
             "The quick brown fox jumps over the lazy dog, unaware that it was being watched by a curious cat on the windowsill.",
             "Although it was raining heavily, the children decided to play outside, splashing in puddles and laughing with joy.",
@@ -19,9 +20,15 @@ public class Main {
     static String sentence;
     static String user_input;
     static long start_dur,end_dur;
-    static int words_count=0;
+    static int word_count=0;
     public static void get_sentence(){
         sentence = sentences[rand.nextInt(sentences.length)];
+    }
+    public static float time_taken(long start, long end){
+        long diff=end-start;
+        float ts=diff/1000.0f;
+        float tm=ts/60.0f;
+        return Math.round(tm*10)/10f;
     }
     public static void welcome(){
         System.out.println("Welcome to the Typing Speed Checker!");
@@ -35,50 +42,49 @@ public class Main {
             if(sc.hasNextLine())
                 break;
         }
-        System.out.print("Game is starting in 3...");
-        Thread.sleep(1000);
-        System.out.print("2...");
-        Thread.sleep(1000);
-        System.out.print("1...");
-        Thread.sleep(1000);
+        sc.nextLine();
+        System.out.println();
+        for(int i=3;i>=1;i--){
+            System.out.print("\rTyping Test is starting in "+i);
+            Thread.sleep(1000);
+        }
+        System.out.print("\rTyping Test has started");
         System.out.println("\nType this: ");
         get_sentence();
-        start_dur=System.currentTimeMillis();
         System.out.println("\""+sentence+"\"");
+        start_dur=System.currentTimeMillis();
         user_input = sc.nextLine();
         end_dur=System.currentTimeMillis();
         System.out.println("You typed: \""+user_input+"\"");
     }
     public static void calculate(){
         int accu_count=0;
-        time=time_taken(start_dur,end_dur)*60.0f;
-        user_input+=" ";
-        for(int i=0;i<user_input.length();i++){
-            char ch = user_input.charAt(i);
-            if(ch==' ')
-                words_count++;
-        }
-        wpm=words_count/time_taken(start_dur,end_dur);
-        for(int i=0;i<sentence.length();i++){
+        time=time_taken(start_dur,end_dur);
+        int len = Math.min(sentence.length(),user_input.length());
+        word_count = user_input.trim().split("\\s+").length;
+        if(time>0)
+            raw_wpm =(int)(word_count/time);
+        else
+            raw_wpm =0;
+        for(int i=0;i<len;i++){
             char user = user_input.charAt(i);
             char comp = sentence.charAt(i);
             if(user==comp)
                 accu_count++;
         }
-        accuracy=(float)accu_count/sentence.length();
-        accuracy=Math.round(accuracy*10)/10f;
+        final_wpm=(int)(accu_count/time);
+        accuracy_total=((float)accu_count/sentence.length())*100.0f;
+        accuracy_total=((int)(accuracy_total *10))/10f;
+        accuracy_input=((float)accu_count/user_input.length())*100.0f;
+        accuracy_input=((int)(accuracy_input *10))/10f;
     }
     public static void display(){
-        System.out.println("Time Taken: "+time);
-        System.out.println("Words Typed: "+words_count);
-        System.out.println("WPM: "+wpm);
-        System.out.println("Accuracy: "+accuracy);
-    }
-    public static float time_taken(long start, long end){
-        long diff=end-start;
-        float ts=diff/1000.0f;
-        float tm=ts/60.0f;
-        return Math.round(tm*10)/10f;
+        System.out.println("\n------------Results------------");
+        System.out.println("Time Taken: "+(time*60.0f)+"s");
+        System.out.println("Words Typed: "+ word_count);
+        System.out.println("Raw WPM: "+ raw_wpm);
+        System.out.println("Full Sentence Accuracy: "+ accuracy_total +"%");
+        System.out.println("Typed Sentence Accuracy: "+ accuracy_input +"%");
     }
     public static void main(String[] args) throws InterruptedException {
         welcome();
